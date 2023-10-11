ba5bcb92ebe5cfcd2e55a27b418b0080048169bc
Hcran18 <hunter.crandall18@gmail.com>
2023-10-10 22:56:40 -0600

    Refactor Presenters: StatusPresenter, StoryPresenter, FeedPresenter

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FeedPresenter.java
@@ -1,62 +1,29 @@
 package edu.byu.cs.tweeter.client.presenter;
 
-import java.util.List;
-
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.StatusService;
-import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
-public class FeedPresenter extends PagedPresenter<Status> {
+public class FeedPresenter extends StatusPresenter {
 
-    public interface View extends PagedPresenter.PagedView<Status> {}
-
-    User user;
-    private StatusService statusService;
+    public interface View extends StatusPresenter.StatusView {}
 
     public FeedPresenter (View view){
         super(view);
-        statusService = new StatusService();
+    }
+
+    @Override
+    protected void doLoad(StatusService statusService, StatusPresenter.StatusServiceObserver statusServiceObserver) {
+        statusService.loadMoreFeed(Cache.getInstance().getCurrUserAuthToken(), user, PAGE_SIZE, lastItems, new FeedPresenter.StatusServiceObserver());
+    }
+
+    @Override
+    protected String getType() {
+        return "feed";
     }
 
     public void loadMoreFeed(User user) {
         this.user = user;
         loadItems();
-    }
-
-    @Override
-    protected void load() {
-        statusService.loadMoreFeed(Cache.getInstance().getCurrUserAuthToken(), user, PAGE_SIZE, lastItems, new FeedPresenter.StatusServiceObserver());
-    }
-
-    private class StatusServiceObserver implements StatusService.StatusObserver {
-
-        @Override
-        public void addMoreStatuses(List<Status> statuses, boolean hasMorePages) {
-            isLoading = false;
-            view.setLoadingFooter(false);
-            FeedPresenter.this.hasMorePages = hasMorePages;
-            lastItems = (statuses.size() > 0) ? statuses.get(statuses.size() - 1) : null;
-            view.addMoreItems(statuses);
-        }
-
-        @Override
-        public void displayError(String message) {
-            isLoading = false;
-            view.setLoadingFooter(false);
-            view.displayMessage(message);
-        }
-
-        @Override
-        public void displayException(Exception ex) {
-            isLoading = false;
-            view.setLoadingFooter(false);
-            view.displayMessage("Failed to get feed because of exception: " + ex.getMessage());
-        }
-
-        @Override
-        public void postSuccess() {
-
-        }
     }
 }
--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/StoryPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/StoryPresenter.java
@@ -1,63 +1,29 @@
 package edu.byu.cs.tweeter.client.presenter;
 
-import java.util.List;
-
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.StatusService;
-import edu.byu.cs.tweeter.client.model.service.UserService;
-import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
 
-public class StoryPresenter extends PagedPresenter<Status> {
+public class StoryPresenter extends StatusPresenter {
 
-    public interface View extends PagedPresenter.PagedView<Status> {}
-
-    User user;
-    private StatusService statusService;
+    public interface View extends StatusPresenter.StatusView {}
 
     public StoryPresenter (View view) {
         super(view);
-        statusService = new StatusService();
     }
 
     @Override
-    protected void load() {
+    protected void doLoad(StatusService statusService, StatusPresenter.StatusServiceObserver statusServiceObserver) {
         statusService.loadMoreStory(Cache.getInstance().getCurrUserAuthToken(), user, PAGE_SIZE, lastItems, new StatusServiceObserver());
+    }
+
+    @Override
+    protected String getType() {
+        return "story";
     }
 
     public void loadMoreStory(User user) {
         this.user = user;
         loadItems();
-    }
-
-    private class StatusServiceObserver implements StatusService.StatusObserver {
-
-        @Override
-        public void addMoreStatuses(List<Status> statuses, boolean hasMorePages) {
-            isLoading = false;
-            view.setLoadingFooter(false);
-            StoryPresenter.this.hasMorePages = hasMorePages;
-            lastItems = (statuses.size() > 0) ? statuses.get(statuses.size() - 1) : null;
-            view.addMoreItems(statuses);
-        }
-
-        @Override
-        public void displayError(String message) {
-            isLoading = false;
-            view.setLoadingFooter(false);
-            view.displayMessage(message);
-        }
-
-        @Override
-        public void displayException(Exception ex) {
-            isLoading = false;
-            view.setLoadingFooter(false);
-            view.displayMessage("Failed to get story because of exception: " + ex.getMessage());
-        }
-
-        @Override
-        public void postSuccess() {
-
-        }
     }
 }
